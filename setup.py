405c10b916e8b882daf476d3f53e23be88c99686
Davis <adavis10006@gamil.com>
2016-05-04 19:09:49 -0600

    Version 0.0.2
    1. Replace all limited API into wordnik API

--- setup.py
+++ setup.py
@@ -16,12 +16,11 @@ def readme():
 
 setup(
     name='exvocabulary',
-    version='0.0.1',
+    version='0.0.2',
     author='Chen, Shang-Kuei',
     author_email='adavis10006@gmail.com',
     description=
     "Module to get part_of_speech, synonym, antonym, vocabulary_example, and hyphenation for a given word. Extend from Vocabulary",
-    #long_description=long_description,
     url='https://github.com/adavis10006/ExVocabulary',
     license='MIT',
     install_requires=[
@@ -29,17 +28,7 @@ setup(
     ],
     ### adding package data to it
     packages=find_packages(exclude=['contrib', 'docs']),
-    ###https://github.com/adavis10006/ExVocabulary/tarball/master
     download_url = 'https://github.com/adavis10006/ExVocabulary/tarball/master',
-    #classifiers = [
-    #    'Intended Audience :: Developers',
-    #    'License :: OSI Approved :: MIT License',
-    #    'Natural Language :: English',
-    #    'Programming Language :: Python',
-    #    'Programming Language :: Python :: 2.7',
-    #    'Programming Language :: Python :: 3',
-    #    'Programming Language :: Python :: 3.4',
-    #],
     test_suite='tests',
     keywords=['Dictionary', 'Vocabulary', 'simple dictionary', 'pydict',
               'dictionary module'])
--- tests/tests.py
+++ tests/tests.py
@@ -14,9 +14,9 @@ class TestModule(unittest.TestCase):
     """Checks for the sanity of all module methods"""
 
     def test_synonym_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.synonym("repudiate")
-        result = '{"verb": ["renounce", "decline", "deny", "refuse", "reject"]}'
+        result = '{"synonym": ["disavow", "renounce", "reject", "disclaim", "divorce"]}'
         middle_val = json.loads(result)
         expected_result = json.dumps(middle_val)
         if sys.version_info[:2] <= (2, 7):
@@ -25,14 +25,14 @@ class TestModule(unittest.TestCase):
             self.assertCountEqual(current_result, expected_result)
 
     def test_synonym_not_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.synonym("sxsw")
         self.assertFalse(current_result)
 
     def test_antonym_valid_phrase_1(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.antonym("love")
-        result = '{"verb": ["hate"], "noun": ["hate"]}'
+        result = '{"antonym": ["malice", "angst", "hate", "spite", "hatred", "despise"]}'
         middle_val = json.loads(result)
         expected_result = json.dumps(middle_val)
         if sys.version_info[:2] <= (2, 7):
@@ -41,9 +41,9 @@ class TestModule(unittest.TestCase):
             self.assertCountEqual(current_result, expected_result)
 
     def test_antonym_valid_phrase_2(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.antonym("respect")
-        result = '{"verb": ["disesteem", "disrespect"], "noun": ["disesteem", "disrespect"]}'
+        result = '{"antonym": ["disrespect", "dis"]}'
         middle_val = json.loads(result)
         expected_result = json.dumps(middle_val)
         if sys.version_info[:2] <= (2, 7):
@@ -52,12 +52,12 @@ class TestModule(unittest.TestCase):
             self.assertCountEqual(current_result, expected_result)
 
     def test_antonym_not_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.antonym("sxsw")
         self.assertFalse(current_result)
 
     def test_partOfSpeech_valid_phrase_1(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.part_of_speech("hello")
         result = '[{"text": "interjection", "example:": "Used to greet someone, answer the telephone, or express surprise.", "seq": 0}]'
         middle_val = json.loads(result)
@@ -68,7 +68,7 @@ class TestModule(unittest.TestCase):
             self.assertCountEqual(current_result, expected_result)
 
     def test_partOfSpeech_valid_phrase_2(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.part_of_speech("rapidly")
         result = '[{"text": "adverb", "example:": "With speed; in a rapid manner.", "seq": 0}]'
         middle_val = json.loads(result)
@@ -79,12 +79,12 @@ class TestModule(unittest.TestCase):
             self.assertCountEqual(current_result, expected_result)
 
     def test_partOfSpeech_not_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.part_of_speech("sxsw")
         self.assertFalse(current_result)
 
     def test_usageExamples_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.vocabulary_example("hillock")
         result = '[{"corpus": "New York Times", "seq": 0, "sentence": "The most immediately captivating of Mr. Sachs\u2019s works is a small, cylindrical fish pond surrounded by a sloping plywood construction, like a flagstone-covered hillock."}, {"corpus": "BBC", "seq": 1, "sentence": "Wakass Haruf taught at Birmingham\'s Golden Hillock Academy - one of the schools caught up in the so-called Trojan Horse affair."}, {"corpus": "BBC", "seq": 2, "sentence": "Nansen Primary and Golden Hillock were also put into special measures."}]'
         middle_val = json.loads(result)
@@ -95,12 +95,12 @@ class TestModule(unittest.TestCase):
             self.assertCountEqual(current_result, expected_result)
 
     def test_usageExamples_not_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.usage_example("lksj")
         self.assertFalse(current_result)
 
     def test_hyphenation_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.hyphenation("hippopotamus")
         result = '[{"seq": 0, "text": "hip", "type": "secondary stress"}, {"seq": 1, "text": "po"}, {"seq": 2, "text": "pot", "type": "stress"}, {"seq": 3, "text": "a"}, {"seq": 4, "text": "mus"}]'
         middle_val = json.loads(result)
@@ -111,7 +111,7 @@ class TestModule(unittest.TestCase):
             self.assertCountEqual(current_result, expected_result)
 
     def test_hyphenation_not_valid_phrase(self):
-        vb = ExVocabulary("tests/api_key.json")
+        vb = ExVocabulary()
         current_result = vb.hyphenation("sxsw")
         self.assertFalse(current_result)
 
